82bfac70f9e8e5c762aff61c9148a22a84983a6d
Matt Delac <matt@delac.io>
2019-10-25 11:55:13 +0100

    Remove console log

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
