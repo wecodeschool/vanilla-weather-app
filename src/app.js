a290cf0e0b67f860ff76873773ca1e8f14934dd7
Matt Delac <matthieu.aussaguel@gmail.com>
2021-03-25 16:46:35 +0000

    Remove console log

--- src/app.js
+++ src/app.js
@@ -68,7 +68,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
