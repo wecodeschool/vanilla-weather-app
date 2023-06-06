9fedee2499b1c8b3164f50e3357b039e64926eda
Matt Delac <matthieu.aussaguel@gmail.com>
2023-06-06 15:43:10 +0100

    declare variable

--- _snapshots/forecast-html-css/src/app.js
+++ _snapshots/forecast-html-css/src/app.js
@@ -31,7 +31,7 @@ function displayTemperature(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
-  celsiusTemperature = response.data.main.temp;
+  let celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
